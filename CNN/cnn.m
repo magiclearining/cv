eef22a1d172719916ab50d27eeaf7a9cd50f2d79
Mr <619500598@qq.com>
2019-06-10 15:51:52 +0800

    为了方便代码在两台电脑写而做的push

--- CNN/cnn.m
+++ CNN/cnn.m
@@ -1,4 +1,4 @@
-%% Conv - ReLu - Pool - Conv - ReLu - Pool - Fc(logistic regression) - sigmon - Softmax 
+% Conv - ReLu - Pool - Conv - ReLu - Pool - Fc(logistic regression) 
 clc; close; clear;
 %[x_train, y_train] = load_datasets();
 
@@ -6,7 +6,7 @@ clc; close; clear;
 conv_layer1_num = 24;
 conv_layer1 = conv_layer_init(3, 3, conv_layer1_num, 10e-4);
 
-conv_layer1_b = 10e-4 * rand(conv_layer1_num,1);
+conv_layer1_b = 10e-4 * (rand(conv_layer1_num,1) - 0.5);
 
 layer1_output = MyReLu(filter3(conv_layer1, conv_layer1_b, x_train{1,1}, 1, 1));
 [Pool_layer1_output, max_position1] = Max_Pool_layer(layer1_output, 2, 1);
@@ -20,4 +20,11 @@ conv_layer2_b = 10e-4 * rand(conv_layer2_num,1);
 layer2_output = MyReLu(filter3(conv_layer2, conv_layer2_b, Pool_layer1_output, 1, 1));
 
 [Pool_layer2_output, max_position2] = Max_Pool_layer(layer2_output, 2, 1);
+
+% logistic regression
+Fc_layer1_input = Pool_layer2_output(:);
+Fc_layer1_input = [1;Fc_layer1_input];
+theta = 10e-4 * (rand(size(Fc_layer1_input, 1) - 0.5));
+lambda = 1;
+[J, grad] = costFunctionReg(theta, Fc_layer1_input, y(1), lambda);
 
